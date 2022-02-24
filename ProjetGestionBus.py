fc84b26d9a0fc98f63535beca0c5761b1ac8348a
ibikiv <ibikivan1@gmail.com>
2022-02-24 15:02:34 +0100

    réalisation de la fonctionnalité de création de passagers

--- ProjetGestionBus.py
+++ ProjetGestionBus.py
@@ -1,5 +1,4 @@
 #On implémente la fonctionnalité de création des bus
-
 def ajoutBus(idBus):
     #on commence par réunir les informations nécessaire à la création du dictionnaire bus
     #on commence par récupérer l'identifiant depuis une varible déclaré à l'avance puis on l'incrémentera après exécution de la fonction
@@ -14,12 +13,13 @@ def ajoutBus(idBus):
     choixPlaces = input("si oui tapez 'Entrer', sinon entrez directement le NOMBRE DE PLACES pour ce bus : ")
     nombreDeplaces = 0
     
+    #S'il n'entre rien on prend le nombre de places par défaut
     if choixPlaces:
         nombreDeplaces = int(choixPlaces)
     else:
         nombreDeplaces = 50
-        
-    bus = "bus" + str(id)
+    
+    #on crée le dictionnaire et on le retourne    
     bus = {"busId" : id,
            "placesMax" : nombreDeplaces,
            "passagers" : passagers}
@@ -28,7 +28,28 @@ def ajoutBus(idBus):
     
     return bus
 
-idBus = 1
+#On implémente la fonctionnalité de création des passager
+def ajoutPassagers(idPassager):
+    #on commence par réunir les informations nécessaire à la création du dictionnaire passager
+    #on commence par récupérer l'identifiant depuis une varible déclaré à l'avance puis on l'incrémentera après exécution de la fonction
+    id = idPassager
+    
+    #puis on récurère dans une variable le nom du passager
+    nomPassager = input("Veuillez entrer le nom de ce passage: ")
+    
+    #et enfin on recupère le poid des bagages du passager en question
+    poidsBagages = input("Veuillez indiquer le poid des bagages de ce passager:  ")
+    
+    #on crée le dictionnaire passager et on le retourne    
+    passager = {"idPassager" : id,
+           "nomPassager" : nomPassager,
+           "poidBagages" : poidsBagages}
+    
+    idPassager += 1
+    
+    return passager
 
-print(ajoutBus(idBus))
-print(idBus)
+idBus = 1
+idPassager = 1
+
+print(ajoutPassagers(idPassager))
